9b954ee0d901140aa0a77d1e3e8ae9cd4a0da111
jiho <tlswlgh120@naver.com>
2023-01-17 22:51:25 +0900

    feat: Update main.rs
    add greet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, guard, HttpRequest, HttpServer, Responder, Route, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", Route::new().guard(guard::Get()).to(greet))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
