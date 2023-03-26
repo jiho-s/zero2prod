9513fa49c448b70e1e5f53ebb405d94cd984080e
jiho <tlswlgh120@naver.com>
2023-03-26 22:39:16 +0900

    refactor: Reject Invalid Subscriber2
    tests refactor

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -8,30 +8,16 @@ use crate::helpers::spawn_app;
 async fn subscribe_returns_a_200_for_valid_from_data() {
     // given
     let app = spawn_app().await;
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_string = configuration.database.with_db();
-
-    let mut connection = PgConnection::connect_with(&connection_string)
-        .await
-        .expect("failed to connect to Postgres");
-
-    let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     // when
-    let response = client
-        .post(format!("{}/subscriptions", &app.address))
-        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request");
+    let response = app.post_subscriptions(body.into()).await;
 
     // then
     assert_eq!(StatusCode::OK, response.status());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&mut connection)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
@@ -43,7 +29,6 @@ async fn subscribe_returns_a_200_for_valid_from_data() {
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
@@ -51,13 +36,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     ];
     for (invalid_body, error_message) in test_cases {
         // Act
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        let response = app.post_subscriptions(invalid_body.into()).await;
         // Assert
         assert_eq!(
             StatusCode::BAD_REQUEST,
@@ -82,13 +61,8 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
 
     for (body, description) in test_cases {
         // when
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        let response = app.post_subscriptions(body.into()).await;
+
         // then
         assert_eq!(
             400,
